58d305abab68bf600b9a8733b4fe76fd7feff814
Janek Bevendorff <janek@refining-linux.org>
2016-07-11 02:21:08 +0200

    Move VisWidget and StarPlot to sub-package

---
+++ vis/VisWidget.py
@@ -0,0 +1,57 @@
+# Copyright (c) 2016 Janek Bevendorff
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene
+from PyQt5.QtGui import QPainter
+from data import Relation
+from abc import abstractmethod
+
+
+class VisWidget(QGraphicsView):
+    def __init__(self):
+        self.scene = QGraphicsScene()
+        super().__init__(self.scene)
+        self.relation = None
+        self.plotPalette = None
+        self.setRenderHint(QPainter.Antialiasing)
+
+    def setRelation(self, rel: Relation):
+        """
+        Initialize widget with L{data.Relation}
+        @param rel: data to be visualized
+        """
+        self.relation = rel
+        self.relation.dataChanged.connect(self.updateWidget)
+
+    def setPlotPalette(self, paletteDict):
+        """
+        Set color palette for lines and points.
+
+        @param paletteDict: dict with class names as keys and L{QColor} objects as values
+        """
+        self.plotPalette = paletteDict
+
+    @abstractmethod
+    def updateWidget(self):
+        """
+        Called when a new relation has been loaded.
+        Implement this method in your subclasses.
+        """
+        pass
